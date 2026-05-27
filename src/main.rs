76e82dadcbe2743ed050d3921906bd48538b7298
saptreekly <jweekly201@gmail.com>
2026-05-27 02:19:34 +0000

    Add aggressive startup telemetry

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ use leptos_router::*;
 use pages::layout::RootLayout;
 use pages::home::HomePage;
 use pages::curriculum::CurriculumPage;
+use web_sys::console;
 
 #[component]
 fn App() -> impl IntoView {
+    console::log_1(&"App component rendering...".into());
     provide_meta_context();
 
     view! {
@@ -18,16 +20,21 @@ fn App() -> impl IntoView {
 
         <Router base="/Research-Publications/">
             <Routes>
-                <Route path="/" view=RootLayout>
+                <Route path="" view=RootLayout>
                     <Route path="" view=HomePage />
                     <Route path="curriculum" view=CurriculumPage />
                 </Route>
+                <Route path="/*any" view=HomePage />
             </Routes>
         </Router>
     }
 }
 
 fn main() {
+    console::log_1(&"!!! MAIN FUNCTION EXECUTING !!!".into());
     console_error_panic_hook::set_once();
-    leptos::mount_to_body(|| view! { <App /> });
+    leptos::mount_to_body(|| {
+        console::log_1(&"Mounting App...".into());
+        view! { <App /> }
+    });
 }
